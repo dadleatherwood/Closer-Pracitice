aed9185f1e456fe6b51df3161dc73556ea46af11
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-05-24 11:18:12 -0500

    created player

---
+++ main.js
@@ -0,0 +1,13 @@
+// create a player constructor that takes the players name as an argument
+
+
+
+
+//player constructor
+function Player (name){
+  this.name = name;
+  //add position
+  this.position = 0;
+}
+
+var player1 = new Player('Jacob');
